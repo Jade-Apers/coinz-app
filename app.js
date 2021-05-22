f3a4e2bcaf68587db65e2d1e4858b17d5e7e465e
Jade Apers <jadeapers@telenet.be>
2021-05-22 10:10:18 +0200

    feat: installed login and signup trial 2

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-
 const apiV1Transfersroutes = require('./routes/api/v1/transfers');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -26,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/transfers', apiV1Transfersroutes);
