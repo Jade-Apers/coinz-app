50eb69240d93dd745cd5e6b715e9418658f397b2
Yanellevdb <77410151+Yanellevdb@users.noreply.github.com>
2021-05-24 12:06:55 +0200

    fix: gulp watch function

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-const { watch, src, dest, series } = require('gulp');
+const {watch} = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
@@ -20,33 +20,26 @@ gulp.task('minify', async function (){
 	.pipe(gulp.dest('dist/html'));
 });
 
-var sass2css= gulp.task ('sass2css', async function(){
-    return src("src/app.scss")
+gulp.task ('sass2css', async function(){
+    return gulp.src('src/**/*.scss')
     .pipe(sass().on('error', sass.logError))
    	.pipe(gulp.dest("./public/stylesheets"))
 });
 
-var image= gulp.task ('image', async function(){
-	gulp.src("src/images/*")
+gulp.task ('image', async function(){
+	gulp.src('src/images/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/img'));
 });
 
-/*
+//watch task
+gulp.task('watch', function(){
+	gulp.watch('src/**/*.scss', gulp.series('sass2css'));
+	gulp.watch('src/images/*', gulp.series('image'));
+});
 
-gulp.task('watch', async function() {
-	gulp.watch("src/app.scss", sass2css);
-	gulp.watch("src/images/*", image)
-})
+gulp.task('default', gulp.parallel('sass2css', 'minify', 'image', 'cssmin', 'watch'));
 
-exports.default = function() {
-    watch("src/.scss", sass2css);
-}
-
-exports.sass2css = sass2css;
-
-
-*/
 
 
 
