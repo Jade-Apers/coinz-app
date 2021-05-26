8687233dcba0997c1391f7603667e7785a5a7979
Yanellevdb <77410151+Yanellevdb@users.noreply.github.com>
2021-05-26 08:51:50 +0200

    fix: gulp minify path

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,7 +1,7 @@
 let btnSignup = document.querySelector('.btnsignup').addEventListener("click", (e) => {
-let email= document.querySelector('.email').value;
-let password = document.querySelector('.password').value;
-let username = document.querySelector('.username').value;
+    let email= document.querySelector('.email').value;
+    let password = document.querySelector('.password').value;
+    let username = document.querySelector('.username').value;
     
 fetch('http://localhost:3000/users/signup', {
     method: "post",
@@ -25,7 +25,6 @@ fetch('http://localhost:3000/users/signup', {
         localStorage.setItem("token", token);
         window.location.href="index.html";
     }
-
     });
 });
 
